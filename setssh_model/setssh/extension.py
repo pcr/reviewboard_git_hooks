2e38cf3303f6699fb2653c492169da213059d719
QingYong <zhangqingyong@kingsoft.com>
2014-02-20 10:47:54 +0800

    Update setssh extension.py

--- setssh_model/setssh/extension.py
+++ setssh_model/setssh/extension.py
@@ -2,10 +2,35 @@ from django.conf import settings
 from reviewboard.extensions.base import Extension
 from reviewboard.extensions.hook import NavigationBarHook
  
+# class SetsshExtension(Extension):
+#     def __init__(self, *args, **kwargs):
+#         super(SetsshExtension, self).__init__(*args, **kwargs)
+#         self.navigationbar_hook = NavigationBarHook(
+#             self,
+#             entries = [
+#                 {
+#                     'label': 'Set ssh',
+#                     'url': settings.SITE_ROOT + 'setssh/',
+#                 },
+#             ]
+#         )
+
+# class SetsshNavigationBarHook(NavigationBarHook):
+            
 class SetsshExtension(Extension):
-    def __init__(self, *args, **kwargs):
-        super(SetsshExtension, self).__init__(*args, **kwargs)
-        self.navigationbar_hook = NavigationBarHook(
+#     def __init__(self, *args, **kwargs):
+#         super(SetsshExtension, self).__init__(*args, **kwargs)
+#         self.navigationbar_hook = NavigationBarHook(
+#             self,
+#             entries = [
+#                 {
+#                     'label': 'Set ssh',
+#                     'url': settings.SITE_ROOT + 'setssh/',
+#                 },
+#             ]
+#         )
+    def initialize(self):
+        NavigationBarHook(
             self,
             entries = [
                 {
